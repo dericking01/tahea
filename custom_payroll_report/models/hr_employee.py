28970c4add0d51e5d89b6b1d32350493b75fb4b8
dericking01 <dericking01@gmail.com>
2025-09-10 08:44:56 +0000

    Clean branch for staging

--- custom_payroll_report/models/hr_employee.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
-    
-    nida_number = fields.Char(string='NIDA Number', size=20)
-    nssf_number = fields.Char(string='NSSF Number', size=20)
-    tin_number = fields.Char(string='TIN Number', size=20)
-    bank_account_number = fields.Char(string='Bank Account Number', size=30)
