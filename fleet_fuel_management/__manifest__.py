280ab1e82818a4f4d6e983007231286f9b88cb91
dericking01 <dericking01@gmail.com>
2026-03-30 15:10:59 +0400

    fleet_fuel_management module added

---
+++ fleet_fuel_management/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Fleet Fuel Management",
+    "version": "1.0",
+    "summary": "Manage fuel logs and link costs to analytic accounts",
+    "category": "Fleet",
+    "author": "Powersoft Solutions",
+    "depends": ["fleet", "hr", "account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
