009d81be99e51b6d5696b3b43aa74f573b7b97a1
dericking01 <dericking01@gmail.com>
2026-05-12 22:07:13 +0400

    feat: Add delivery note title module with report customization

---
+++ delivery_note_title/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Delivery Note Title',
+    'version': '18.0.1.0.0',
+    'summary': 'Rename delivery slip title to DELIVERY NOTE',
+    'category': 'Inventory/Inventory',
+    'author': 'Primesoft Technologies',
+    'license': 'LGPL-3',
+    'depends': ['stock'],
+    'data': [
+        'views/report_delivery_note.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
